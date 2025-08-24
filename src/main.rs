baf2214d6c225181ac6551bb174624c2b9f259a6
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-08-24 10:33:34 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,25 @@ use memmap2::{Mmap, MmapOptions};
 use std::{
     fs::{File, metadata},
     io::{BufReader, Read},
+    rc::Rc,
     time::{SystemTime, UNIX_EPOCH},
 };
 
-struct TokenIndex<'a> {
-    token_str: &'a str,
+#[derive(Clone)]
+struct TokenIndex {
+    token_str: Rc<String>,
     id: usize,
 }
 
 struct Tokenizer {
-    // Note: use ascii string (char) instead of unicode str.
-    vocab: Vec<String>,
+    // NOTE: use ascii string (char) instead of unicode str.
+    vocab: Vec<Rc<String>>,
     vocab_scores: Vec<f32>,
-    //sorted_vocab: Vec<TokenIndex<'a>>,
+    /// sorted_vocab refers to vocab strings.
+    sorted_vocab: Vec<TokenIndex>,
     vocab_size: usize,
     max_token_length: usize,
-    //Note: use memory-continuous representation of those byte pieces, with simpler strings.
+    // NOTE: use memory-continuous representation of those byte pieces, with simpler strings.
     byte_pieces: [Box<String>; 256],
 }
 
@@ -27,7 +30,7 @@ impl Tokenizer {
         let byte_pieces: [Box<String>; 256] =
             std::array::from_fn(|i| Box::new((i as u8 as char).to_string()));
         //dbg!(&byte_pieces);
-        let mut vocab = vec!["".to_string(); vocab_size];
+        let mut vocab: Vec<Rc<String>> = (0..vocab_size).map(|_| Rc::new("".to_string())).collect();
         let mut vocab_scores = vec![0f32; vocab_size];
         let file = File::open(path).unwrap();
         let mut reader = BufReader::new(file);
@@ -55,16 +58,39 @@ impl Tokenizer {
             // dbg!(i, score, &len, &s);
             // dbg!(score);
             // eprintln!("{i} {score} {len} {s}");
-            vocab[i] = s;
+            vocab[i] = Rc::new(s);
+        }
+
+        let mut sorted_vocab: Vec<TokenIndex> = (0..vocab_size)
+            .map(|id| TokenIndex {
+                token_str: Rc::clone(&vocab[id]),
+                id,
+            })
+            .collect();
+        sorted_vocab.sort_by(|a, b| a.token_str.partial_cmp(&b.token_str).unwrap());
+
+        for s in &sorted_vocab {
+            eprintln!("{}, {}", s.token_str, s.id)
         }
 
         Tokenizer {
             vocab_size,
             vocab,
             vocab_scores,
+            sorted_vocab,
             byte_pieces,
             max_token_length,
         }
+    }
+
+    /// find the perfect match for str in vocab
+    fn str_lookup(&self, needle: &str) -> Option<TokenIndex> {
+        // int str_lookup(char *str, TokenIndex *sorted_vocab, int vocab_size)
+        let i_found = self
+            .sorted_vocab
+            .binary_search_by(|probe| probe.token_str.as_str().cmp(needle))
+            .ok()?;
+        Some(self.sorted_vocab.get(i_found).unwrap().clone())
     }
 }
 
@@ -393,7 +419,187 @@ fn generate(
     prompt: String,
     steps: usize,
 ) {
+    // encode the (string) prompt into tokens sequence
+    //num_prompt_tokens: usize = 0;
+    //int* prompt_tokens = (int*)malloc((strlen(prompt)+3) * sizeof(int)); // +3 for '\0', ?BOS, ?EOS
+    //encode(tokenizer, prompt, 1, 0, prompt_tokens, &num_prompt_tokens);
+    //if (num_prompt_tokens < 1) {
+    //    fprintf(stderr, "something is wrong, expected at least 1 prompt token\n");
+    //    exit(EXIT_FAILURE);
+    //}
     todo!("HERE")
+    /*
+
+    // start the main loop
+    long start = 0;  // used to time our code, only initialized after first iteration
+    int next;        // will store the next token in the sequence
+    int token = prompt_tokens[0]; // kick off with the first token in the prompt
+    int pos = 0;     // position in the sequence
+    while (pos < steps) {
+
+        // forward the transformer to get logits for the next token
+        float* logits = forward(transformer, token, pos);
+
+        // advance the state machine
+        if (pos < num_prompt_tokens - 1) {
+            // if we are still processing the input prompt, force the next prompt token
+            next = prompt_tokens[pos + 1];
+        } else {
+            // otherwise sample the next token from the logits
+            next = sample(sampler, logits);
+        }
+        pos++;
+
+        // data-dependent terminating condition: the BOS (=1) token delimits sequences
+        if (next == 1) { break; }
+
+        // print the token as string, decode it with the Tokenizer object
+        char* piece = decode(tokenizer, token, next);
+        safe_printf(piece); // same as printf("%s", piece), but skips "unsafe" bytes
+        fflush(stdout);
+        token = next;
+
+        // init the timer here because the first iteration can be slower
+        if (start == 0) { start = time_in_ms(); }
+    }
+    printf("\n");
+
+    // report achieved tok/s (pos-1 because the timer starts after first iteration)
+    if (pos > 1) {
+        long end = time_in_ms();
+        fprintf(stderr, "achieved tok/s: %f\n", (pos-1) / (double)(end-start)*1000);
+    }
+     */
+}
+
+enum BosEos {
+    /// Beginning of string
+    BOS,
+    // Neither BOS nor EOS
+    MID,
+    /// End of string
+    EOS,
+}
+
+/// encode the string text (input) into an upper-bound preallocated tokens[] array
+/// bos != 0 means prepend the BOS token (=1), eos != 0 means append the EOS token (=2)
+fn encode(t: &Tokenizer, text: &str, bos_eos: BosEos) {
+    // void encode(Tokenizer* t, char *text, int8_t bos, int8_t eos, int *tokens, int *n_tokens)
+    // int* prompt_tokens = (int*)malloc((strlen(prompt)+3) * sizeof(int)); // +3 for '\0', ?BOS, ?EOS
+
+    // TODO return tokens, return n_tokens
+    /*
+        /// // create a temporary buffer that will store merge candidates of always two consecutive tokens
+        /// // *2 for concat, +1 for null terminator +2 for UTF8 (in case max_token_length is 1)
+        /// char* str_buffer = malloc((t->max_token_length*2 +1 +2) * sizeof(char));
+        /// size_t str_len = 0;
+
+        /// // start at 0 tokens
+        /// *n_tokens = 0;
+
+        // TODO add BOS
+        // add optional BOS (=1) token, if desired
+        if (bos) tokens[(*n_tokens)++] = 1;
+
+        // add_dummy_prefix is true by default
+        // so prepend a dummy prefix token to the input string, but only if text != ""
+        // TODO: pretty sure this isn't correct in the general case but I don't have the
+        // energy to read more of the sentencepiece code to figure out what it's doing
+
+        // TODO Add dummy prefix
+        if (text[0] != '\0') {
+            int dummy_prefix = str_lookup(" ", t->sorted_vocab, t->vocab_size);
+            tokens[(*n_tokens)++] = dummy_prefix;
+        }
+
+        /// // Okay UTF-8 time. This will get messy. Here is the reference from Wikipedia:
+        /// // Code point ↔ UTF-8 conversion
+        /// // First code point	Last code point	Byte 1	Byte 2	Byte 3	Byte 4
+        /// // U+0000	U+007F	    0xxxxxxx
+        /// // U+0080	U+07FF	    110xxxxx	10xxxxxx
+        /// // U+0800	U+FFFF	    1110xxxx	10xxxxxx	10xxxxxx
+        /// // U+10000	U+10FFFF    11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
+
+        // process the raw (UTF-8) byte sequence of the input string
+        for (char *c = text; *c != '\0'; c++) {
+
+            /// // reset buffer if the current byte is ASCII or a leading byte
+            /// // 0xC0 is 11000000, so (*c & 0xC0) keeps the first 2 bits and zeros the rest
+            /// // 0x80 is 10000000
+            /// // in UTF-8, all continuation bytes start with "10" in first two bits
+            /// // so in English this is: "if this byte is not a continuation byte"
+            /// if ((*c & 0xC0) != 0x80) {
+            ///     // this byte must be either a leading byte (11...) or an ASCII char (0x...)
+            ///     // => reset our location, as we're starting a new UTF-8 codepoint
+            ///     str_len = 0;
+            /// }
+
+            /// // append the current byte to the buffer
+            /// str_buffer[str_len++] = *c; // ++ is post-increment, incremented after this line
+            /// str_buffer[str_len] = '\0';
+
+            /// // while the next character is a continuation byte, continue appending
+            /// // but if there are too many of them, just stop to avoid overruning str_buffer size.
+            /// if ((*(c+1) & 0xC0) == 0x80 && str_len < 4) {
+            ///     continue;
+            /// }
+
+            // ok c+1 is not a continuation byte, so we've read in a full codepoint
+            // decode codepoint by codepoint
+            int id = str_lookup(str_buffer, t->sorted_vocab, t->vocab_size);
+
+            if (id != -1) {
+                // we found this codepoint in vocab, add it as a token
+                tokens[(*n_tokens)++] = id;
+            } else {
+                // byte_fallback encoding: just encode each byte as a token
+                // +3 is here because the first 3 vocab elements are <unk>, <s>, </s>
+                // so the individual bytes only start at index 3
+                for (int i=0; i < str_len; i++) {
+                    tokens[(*n_tokens)++] = (unsigned char)str_buffer[i] + 3;
+                }
+            }
+            str_len = 0; // protect against a sequence of stray UTF8 continuation bytes
+        }
+
+        // merge the best consecutive pair each iteration, according the scores in vocab_scores
+        while (1) {
+            float best_score = -1e10;
+            int best_id = -1;
+            int best_idx = -1;
+
+            for (int i=0; i < (*n_tokens-1); i++) {
+                // check if we can merge the pair (tokens[i], tokens[i+1])
+                sprintf(str_buffer, "%s%s", t->vocab[tokens[i]], t->vocab[tokens[i+1]]);
+                int id = str_lookup(str_buffer, t->sorted_vocab, t->vocab_size);
+                if (id != -1 && t->vocab_scores[id] > best_score) {
+                    // this merge pair exists in vocab! record its score and position
+                    best_score = t->vocab_scores[id];
+                    best_id = id;
+                    best_idx = i;
+                }
+            }
+
+            if (best_idx == -1) {
+                break; // we couldn't find any more pairs to merge, so we're done
+            }
+
+            // merge the consecutive pair (best_idx, best_idx+1) into new token best_id
+            tokens[best_idx] = best_id;
+            // delete token at position best_idx+1, shift the entire sequence back 1
+            for (int i = best_idx+1; i < (*n_tokens-1); i++) {
+                tokens[i] = tokens[i+1];
+            }
+            (*n_tokens)--; // token length decreased
+        }
+
+        // add optional EOS (=2) token, if desired
+        if (eos) tokens[(*n_tokens)++] = 2;
+
+        free(str_buffer);
+    }
+
+         */
 }
 
 fn main() {
