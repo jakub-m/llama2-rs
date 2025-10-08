417d66f379b52c93ed20f822fb55e0fd7a7f5c09
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-10-08 08:55:56 +0200

    move matmul to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod logging;
+pub mod matmul;
 pub mod metal;
--- src/main.rs
+++ src/main.rs
@@ -1076,47 +1076,6 @@ fn softmax(x: &mut [f32], size: usize) {
     }
 }
 
-/// W (d,n) @ x (n,) -> xout (d,)
-/// by far the most amount of time is spent inside this little function
-/// Here d is used only for extra boundary checks, xout slice should have the correct length.
-fn matmul(xout: &mut [f32], x: &[f32], w: &[f32], n: usize, d: usize) {
-    trace!("matmul n={n} d={d}");
-    assert_eq!(
-        xout.len(),
-        d,
-        "expected different size of the slice. xout.len()={}, expected d={}",
-        xout.len(),
-        d
-    );
-    // Purely serial option
-    //   xout.iter_mut().enumerate().for_each(|(k, xout_val)| {
-    // Rayon par_iter_mut
-    //   xout.par_iter_mut().enumerate().for_each(|(k, xout_val)| {
-    // Par Bridge
-    //   xout.iter_mut().enumerate().par_bridge().for_each(|(k, xout_val)| {
-    xout.par_iter_mut()
-        .with_min_len(15)
-        .enumerate()
-        .for_each(|(k, xout_val)| {
-            let mut val: f32 = 0.0;
-            for i in 0..n {
-                val += w[k * n + i] * x[i];
-            }
-            *xout_val = val;
-        });
-}
-
-//fn slicecpy<T: Clone>(target: &mut Vec<T>, source: &[T], n_t: usize) {
-//    let capacity_init = target.capacity();
-//    target.clear();
-//    target.extend_from_slice(&source[..n_t]);
-//    assert_eq!(
-//        capacity_init,
-//        target.capacity(),
-//        "Error! the vector was reallocated but it should not have"
-//    );
-//}
-
 /// - n_t - number of elements to copy from source to target.
 fn slicecpy<T: Clone>(target: &mut [T], source: &[T], n_t: usize) {
     target[..n_t].clone_from_slice(&source[..n_t]);
@@ -1290,7 +1249,7 @@ fn report_tok_per_sec(
 #[cfg(test)]
 mod tests {
     use super::debug;
-    use crate::{AddBos, ProbIndex, Tokenizer, advance_slice, matmul, rmsnorm, slicecpy};
+    use crate::{AddBos, ProbIndex, Tokenizer, advance_slice, rmsnorm, slicecpy};
     use std::sync::{LazyLock, Mutex};
 
     // TOKENIZER tries to share the Tokenizer across test cases.
@@ -1368,58 +1327,6 @@ mod tests {
                 "actual {o:?}\nexpected {expected:?}"
             );
         }
-    }
-
-    #[test]
-    fn test_matmul() {
-        let w: Vec<f32> = vec![
-            0.1, 0.2, 0.3, // r0
-            0.4, 0.5, 0.6, // r1
-        ];
-        let x: Vec<f32> = vec![
-            1.0, //r0
-            2.0, //r1
-            3.0, //r2
-        ];
-
-        let mut xout: Vec<f32> = vec![0.0; 2];
-
-        matmul(&mut xout, &x, &w, 3, 2);
-        assert_eq!(
-            xout,
-            vec![
-                0.1 * 1.0 + 0.2 * 2.0 + 0.3 * 3.0, //r0
-                0.4 * 1.0 + 0.5 * 2.0 + 0.6 * 3.0, //r1
-            ]
-        );
-        assert_eq!(xout.len(), 2);
-    }
-
-    #[test]
-    fn tests_matmul_w_4_3() {
-        let input_w: Vec<f32> = vec![
-            1., 2., 3., //
-            4., 5., 6., //
-            7., 8., 9., //
-            10., 11., 12., //
-        ];
-        let input_x: Vec<f32> = vec![
-            1., //
-            2., //
-            3., //
-        ];
-        let mut output: Vec<f32> = vec![0.; 4];
-        matmul(&mut output, &input_x, &input_w, 3, 4);
-
-        assert_eq!(
-            output,
-            vec![
-                1. * 1. + 2. * 2. + 3. * 3.,    //
-                4. * 1. + 5. * 2. + 6. * 3.,    //
-                7. * 1. + 8. * 2. + 9. * 3.,    //
-                10. * 1. + 11. * 2. + 12. * 3., //
-            ]
-        );
     }
 
     #[test]
